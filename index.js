ac8d270b77209098f549f40945f298e38a94e44b
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-05 18:23:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ class Passenger {
 
     store.passengers.push(this);
   }
+
+  trips(){
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id
+    })
+  }
 }
 
 class Trip {
